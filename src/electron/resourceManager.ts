a152ac32cdf5a28ac31fa4f7a330993b14771be6
Sundze <keyonmwesundze@mail.com>
2025-03-27 10:57:15 +0100

    ...

--- src/electron/resourceManager.ts
+++ src/electron/resourceManager.ts
@@ -1,4 +1,5 @@
 import osUtils from "os-utils";
+import fs from "fs";
 
 const POLLING_INTERVAL = 500; // runs twice/sec
 
@@ -6,7 +7,8 @@ export function pollResources() {
   setInterval(async () => {
     const cpuUsage = await getCpuUsage();
     const ramUsage = getRamUsage();
-    console.log({ cpuUsage, ramUsage });
+    const storageData = getStorageData();
+    console.log({ cpuUsage, ramUsage, storageData: storageData.usage });
   }, POLLING_INTERVAL);
 }
 
@@ -18,4 +20,17 @@ function getCpuUsage() {
 
 function getRamUsage() {
   return 1 - osUtils.freememPercentage();
+}
+
+function getStorageData() {
+  // requires from node 18
+  const stats = fs.statfsSync(process.platform === "win32" ? "C://" : "/");
+  const total = stats.bsize * stats.blocks; // size of 1 block * num of blocks
+  const free = stats.bsize * stats.bfree; // size of 1 block * num of free blocks
+
+  return {
+    // 10^9 = 1GB
+    total: Math.floor(total / 1_000_000_000),
+    usage: 1 - free / total,
+  };
 }
