ee72b17edbddfb71e77bc7dfe3c73e9cdd05e044
Sundze <keyonmwesundze@mail.com>
2025-03-30 12:27:26 +0100

    ...

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,12 +1,17 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, ipcMain } from "electron";
 import path from "path";
 import { isDev } from "./util.js";
-import { pollResources } from "./resourceManager.js";
+import { getStaticData, pollResources } from "./resourceManager.js";
+import { getPreloadPath } from "./pathResolver.js";
 
 // type test = string;
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+  });
   //app.getAppPath() -> gets the correct path and loads the index file for our app, regardless where the project is run
   //path.join() -> vital for windows OS, cuz it doesn't accept "/" in path
   if (isDev()) {
@@ -14,5 +19,9 @@ app.on("ready", () => {
   } else {
     mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
   }
-  pollResources();
+  pollResources(mainWindow);
+
+  ipcMain.handle("getStaticData", () => {
+    return getStaticData();
+  });
 });
